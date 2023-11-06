64bbbf097ea4402711ab7ca2c7ca7666ee3974bc
Evan Martinez <emmplse821@gmail.com>
2023-11-05 21:43:59 -0800

    Added input box to html and updated JS

--- post.js
+++ post.js
@@ -1,11 +1,31 @@
 const url1 = 'https://jsonplaceholder.typicode.com/users';
 const name1 = document.querySelector('#newName');
+const newName = document.getElementById('name');
+const newEmail = document.getElementById('email');
+const addPerson = document.querySelector('.newPerson');
 
 //  -------------- This was the data object gathered from https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-fetch-api-to-get-data#step-3-handling-post-requests.
 // ---------------- I decided to add an email and an extra key value pair.  I displayed posted data on the HMTL file.
 // let data = {
 //     name: 'Sammy'
 //   }
+
+let unit = 1;
+
+addPerson.addEventListener('click',()=>{
+  unit = unit + 1;
+  // data[unit].name='john';
+  // data[unit].email='fff';
+  let newObject = {
+    name:newName.value,
+    email:newEmail.value
+}
+  data.push(newObject);
+  console.log(data);
+});
+
+// 1. To have new input POST-ed and also have it displayed
+// 2. Figure out how to know if it has been posted. 
 
 let data = [
   {
@@ -36,3 +56,21 @@ fetch(request)
     
   });
 
+// fetch(request)
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((postedData) => {
+//     postedData.forEach((item) => {
+//       let name = item.name;
+//       let email = item.email;
+
+//       let nameElement = document.createElement('div');
+//       nameElement.innerHTML = `<span>${name}</span> - <span>${email}</span>`;
+
+//       name1.appendChild(nameElement);
+//     });
+//   })
+//   .catch((error) => {
+//     console.error('Error:', error);
+//   });
